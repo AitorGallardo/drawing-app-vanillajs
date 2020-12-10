e4b5a1523a614f7457c9338245f9af51d6773cfa
aitor <gmaitor@outlook.com>
2020-12-10 15:48:13 +0100

    stroke selector, color selector

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 
 let x = 0;
 let y = 0;
+const size = 5;
 let mousedown = false;
 
 canvas.addEventListener('mousedown', (e) => {
@@ -19,23 +20,33 @@ canvas.addEventListener('mouseup', (e) => {
 
 canvas.addEventListener('mousemove', (e) => {
 
-    if(mousedown){
+    if (mousedown) {
         const x1 = e.offsetX;
         const y1 = e.offsetY;
-        
-        drawLine(x, y, x1, y1)
+
+        drawCircle(x,y,size)
+        drawLine(x, y, x1, y1,size)
         x = x1;
         y = y1;
     }
 })
 
 
-function drawLine(x0, y0, x1, y1) {
+function drawLine(x0, y0, x1, y1, size) {
     ctx.beginPath();
+    ctx.lineWidth = size*2;
     ctx.moveTo(x0, y0);
     ctx.lineTo(x1, y1);
     ctx.stroke();
 }
+
+function drawCircle(x,y,size) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+
 
 function changeColor() {
     ctx.strokeStyle = "#FF0000";
