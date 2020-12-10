2dae85b9682afea507c466a966140bc045f84387
aitor <gmaitor@outlook.com>
2020-12-10 12:18:40 +0100

    drawing on mouse down

--- script.js
+++ script.js
@@ -3,23 +3,40 @@ const ctx = canvas.getContext("2d");
 
 let x = 0;
 let y = 0;
-canvas.addEventListener('mousemove',(e)=>{
-    const x1 = e.offsetX;
-    const y1 = e.offsetY;
+let mousedown = false;
 
-    drawLine(x,y,x1,y1)
-    x = x1;
-    y = y1;
+canvas.addEventListener('mousedown', (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+
+    mousedown = true;
+})
+canvas.addEventListener('mouseup', (e) => {
+    mousedown = false;
 })
 
 
-function drawLine(x0,y0,x1,y1) {
+
+canvas.addEventListener('mousemove', (e) => {
+
+    if(mousedown){
+        const x1 = e.offsetX;
+        const y1 = e.offsetY;
+        
+        drawLine(x, y, x1, y1)
+        x = x1;
+        y = y1;
+    }
+})
+
+
+function drawLine(x0, y0, x1, y1) {
     ctx.beginPath();
     ctx.moveTo(x0, y0);
     ctx.lineTo(x1, y1);
     ctx.stroke();
 }
 
-function changeColor(){
+function changeColor() {
     ctx.strokeStyle = "#FF0000";
 }
