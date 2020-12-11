692e62875024f477973a36faf72754d4f43ff379
aitor <gmaitor@outlook.com>
2020-12-11 22:58:06 +0100

    selecting buttons

--- script.js
+++ script.js
@@ -3,23 +3,27 @@ import Canvas from './canvas.js'
 const canvas = document.querySelector("canvas");
 
 const stroke_sizes_buttons = document.querySelectorAll('.stroke-size-selectors > button')
-const colors_buttons = document.querySelectorAll('.grid-color > *')
+const color_buttons = document.querySelectorAll('.grid-color > *')
 const clear_button = document.querySelector('#clear')
 
 
-const canvas_buttons = {stroke_sizes_buttons,colors_buttons, clear_button}
+const canvas_buttons = {stroke_sizes_buttons,color_buttons, clear_button}
 const _Canvas = new Canvas(canvas, canvas_buttons);
 
 
-colors_buttons.forEach((color)=>{
-    color.addEventListener('click',()=>{
-        _Canvas.pickColor(color.id)
+color_buttons.forEach((color_button)=>{
+    color_button.addEventListener('click',()=>{
+        removeSelectedClass(color_buttons);
+        color_button.classList.add('selected-button')
+        _Canvas.pickColor(color_button.id)
     })
 })
 
-stroke_sizes_buttons.forEach((stroke_size)=>{
-    const size = parseInt(stroke_size.id.split('-')[1]);
-    stroke_size.addEventListener('click',()=>{
+stroke_sizes_buttons.forEach((stroke_size_button)=>{
+    const size = parseInt(stroke_size_button.id.split('-')[1]);
+    stroke_size_button.addEventListener('click',()=>{
+        removeSelectedClass(stroke_sizes_buttons);
+        stroke_size_button.classList.add('selected-button')
         _Canvas.size = size;
     })
 })
@@ -27,3 +31,9 @@ stroke_sizes_buttons.forEach((stroke_size)=>{
 clear_button.addEventListener('click',()=>{
     _Canvas.clear();
 })
+
+function removeSelectedClass(collection){
+    collection.forEach((ea)=>{
+        ea.classList.remove('selected-button');
+    })
+}
